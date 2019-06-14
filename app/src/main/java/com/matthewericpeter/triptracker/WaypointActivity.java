8e36dacd43db7c7a226d4759519046cefc8e7078
Eric Sparrow <ericsparro@gmail.com>
2019-06-13 21:27:58 -0400

    Firebase DB setup.... may need to remake waypoint activity ( or at least remove all the extras that came with..)

--- app/src/main/java/com/matthewericpeter/triptracker/WaypointActivity.java
+++ app/src/main/java/com/matthewericpeter/triptracker/WaypointActivity.java
@@ -1,13 +1,23 @@
 package com.matthewericpeter.triptracker;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class WaypointActivity extends AppCompatActivity {
+
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference wayRef = rootRef.child("Waypoints");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,25 @@ public class WaypointActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        wayRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //waypoint newWaypoint = dataSnapshot.getValue(waypoint.class);
+                String text = dataSnapshot.getValue(String.class);
+                //txtView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
