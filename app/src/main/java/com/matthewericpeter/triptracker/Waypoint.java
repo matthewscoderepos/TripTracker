af67be6241fd5ba7a8b75c99654ecf21798321df
Eric Sparrow <ericsparro@gmail.com>
2019-06-15 17:56:39 -0400

    Firebase DB setup, initial testing of database functions here work, will continue adding functions to properly use the database

--- app/src/main/java/com/matthewericpeter/triptracker/Waypoint.java
+++ app/src/main/java/com/matthewericpeter/triptracker/Waypoint.java
@@ -4,7 +4,7 @@ public class Waypoint {
     public String name;
     public double latitude;
     public double longitude;
-    Waypoint(String _name, double _lat, double _lon) {
+    Waypoint(String _name, double _lat, double _lon){
         name = _name;
         latitude = _lat;
         longitude = _lon;
