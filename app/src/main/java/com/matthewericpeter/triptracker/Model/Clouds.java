e60e2360746c3feec38ccb167c55650070c0a161
pstilian <pstilian111@gmail.com>
2019-06-17 17:25:58 -0400

    added weather model class

---
+++ app/src/main/java/com/matthewericpeter/triptracker/Model/Clouds.java
@@ -0,0 +1,15 @@
+package com.matthewericpeter.triptracker.Model;
+
+public class Clouds {
+    private int all;
+
+    public Clouds(){}
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
---
+++ app/src/main/java/com/matthewericpeter/triptracker/Model/Main.java
@@ -0,0 +1,51 @@
+package com.matthewericpeter.triptracker.Model;
+
+public class Main {
+    private double temp;
+    private int pressure;
+    private int humidity;
+    private double temp_min;
+    private int temp_max;
+
+    public Main() {}
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getTemp_min() {
+        return temp_min;
+    }
+
+    public void setTemp_min(double temp_min) {
+        this.temp_min = temp_min;
+    }
+
+    public int getTemp_max() {
+        return temp_max;
+    }
+
+    public void setTemp_max(int temp_max) {
+        this.temp_max = temp_max;
+    }
+}
